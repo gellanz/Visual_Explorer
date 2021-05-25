be8f263a7443e2af7a0e792c26856c92ba442f0a
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-05-25 13:40:15 -0500

    orden de comandos corregidos

--- Vagrant/Scripts/boot_db_first_time.sh
+++ Vagrant/Scripts/boot_db_first_time.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd
-cd /prueba
 source mysql-faster-imports.sh
 mysqlOptimizeForImports
+cd 
+cd /prueba
 mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS document_analyzer"
 for file in *.sql; do
 mysql -u root -proot document_analyzer < $file
