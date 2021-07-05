517cabc6bd93de5ed44453f3caf225ec948baced
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-07-05 02:28:05 -0500

    beats added

--- Scripts/start_ELK.sh
+++ Scripts/start_ELK.sh
@@ -4,5 +4,6 @@ sudo sysctl -w vm.max_map_count=262144
 sudo docker start elasticsearch
 sudo docker start kibana
 sudo docker ps
-
+sudo service metricbeat start
+sudo service heartbeat-elastic start
 
