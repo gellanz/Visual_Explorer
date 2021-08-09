9ab280afb82b5c5ca78f12609da87b3f29810f5f
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-08-09 01:54:07 -0500

    updated for containers names

--- Scripts/start_ELK.sh
+++ Scripts/start_ELK.sh
@@ -3,7 +3,7 @@ cd /docker
 sudo sysctl -w vm.max_map_count=262144
 sudo docker start elasticsearch
 sudo docker start kibana
+sudo docker start metricbeat
+sudo docker start heartbeat
 sudo docker ps
-sudo service metricbeat start
-sudo service heartbeat-elastic start
 
--- Scripts/stop_ELK.sh
+++ Scripts/stop_ELK.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd /docker 
-sudo docker stop docker_metricbeat_1
 sudo docker stop elasticsearch
+sudo docker stop metricbeat
+sudo docker stop heartbeat
 sudo docker stop kibana
 sudo docker ps
 
