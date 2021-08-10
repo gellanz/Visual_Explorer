2a6f8eede799e520a5171a707a2762f205ebd112
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-08-10 13:53:26 -0500

    added stop and start of elk stack

--- Scripts/boot_db_first_time.sh
+++ Scripts/boot_db_first_time.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 source mysql-faster-imports.sh
 mysqlOptimizeForImports
+source /scripts/stop_ELK.sh
 cd 
 cd /sql
 mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS document_analyzer"
 for file in *.sql; do
-mysql -u root -proot document_analyzer < $file
+    echo "exporting $file"
+    mysql -u root -proot document_analyzer < $file
 done
 mysqlDefaultSettings
+source /scripts/start_ELK.sh
