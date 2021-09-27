01d04c1dc6596b04667f7103dfd0c7ca8fb542f0
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-09-27 01:16:46 -0500

    indexes updated

--- Scripts/es_index_creation.py
+++ Scripts/es_index_creation.py
@@ -26,6 +26,30 @@ news_body = {
                 "content": {
                     "type": "text",
                     "analyzer": "whitespace"
+                },
+                # Los siguientes campos son de los agregados posteriormente. Manual tecnico M. Yadira pag. 12
+                "categoryKW": {
+                     "type": "keyword"
+                },
+                "contentWsTk": {
+                     "type": "text",
+                     "analyzer": "whitespace" 
+                },
+                "documentURL": {
+                     "type": "text"
+                },
+                "imageURL": { 
+                     "type": "text" 
+                },
+                "contentFD": { 
+                     "type": "text",
+                     "fielddata": True 
+                },
+                "categoryMLP": { 
+                     "type": "keyword" 
+                },
+                "sourceNameKW": {
+                     "type": "keyword" 
                 }
             }
         }
@@ -46,7 +70,14 @@ entities_body = {
                 },
                 "catEntity": {
                     "type": "text"
-                }
+                },
+                # Los siguientes campos son de los agregados posteriormente. Manual tecnico M. Yadira pag. 12
+                "catEntityKW": { 
+                    "type": "keyword" 
+                },
+                "entityKW": { 
+                    "type": "keyword" 
+                },
             }
         }
 }
@@ -55,3 +86,6 @@ print("Creating 'document_analyzer' index")
 es.indices.create(index = 'document_analyzer', body = news_body)
 print("Creating 'named_entities' index")
 es.indices.create(index = 'named_entities', body = entities_body)
+
+# http://localhost:5601/app/management/data/index_management/indices
+# http://localhost:5601/app/dev_tools#/console?load_from=https:/www.elastic.co/guide/en/elasticsearch/reference/current/snippets/2215.console
